452e66e3ee54deac40b7746c34ee2979b6087d5a
standa-cerny <33260301+standa-cerny@users.noreply.github.com>
2017-11-04 15:40:17 +0100

    Initial commit

---
+++ app/src/main/java/net/standadev/coffeecounter/SelectUserActivity.java
@@ -0,0 +1,13 @@
+package net.standadev.coffeecounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_user);
+    }
+}
