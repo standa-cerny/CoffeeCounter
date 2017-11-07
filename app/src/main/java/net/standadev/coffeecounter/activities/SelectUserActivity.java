36c90e7e4a8674c9051f1e45e94ad078a54e704b
standa-cerny <33260301+standa-cerny@users.noreply.github.com>
2017-11-07 22:03:01 +0100

    Main menu added. ChangeUserActivity stub added.

--- app/src/main/java/net/standadev/coffeecounter/activities/SelectUserActivity.java
+++ app/src/main/java/net/standadev/coffeecounter/activities/SelectUserActivity.java
@@ -1,7 +1,11 @@
 package net.standadev.coffeecounter.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.GridView;
 
 import net.standadev.coffeecounter.R;
@@ -10,6 +14,8 @@ import net.standadev.coffeecounter.data.Counter;
 import net.standadev.coffeecounter.data.User;
 
 import java.util.ArrayList;
+
+
 
 public class SelectUserActivity extends AppCompatActivity {
     GridView userGrid;
@@ -33,4 +39,31 @@ public class SelectUserActivity extends AppCompatActivity {
         super.onResume();
         userGridAdapter.notifyDataSetChanged();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);//Menu Resource, Menu
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Context context = SelectUserActivity.this;
+        switch (item.getItemId()) {
+            case R.id.menu_add_user:
+
+                Intent intent = new Intent(context, ChangeUserActivity.class);
+                intent.putExtra(Counter.USER_ID, 0);
+                context.startActivity(intent);
+                return true;
+            case R.id.menu_ingredients:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
